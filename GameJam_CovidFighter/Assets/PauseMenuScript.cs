52a26916721ef9387c012b76ec8ab49c9d5e7909
polple <66416994+polple@users.noreply.github.com>
2020-12-01 19:27:41 +0100

    Added MainMenu, PauseMenu Object and EndMenu
    Several asset files were added including a pauseMenu prefab (requires an event system to be in the room for functionaility). as well as some sprites for trees and rocks, a MenuSoundEffect and MenuParticles.
    Also includes scenes for the MainMenu and WinScreen menu with functionality pretty much complete (some functions can only be used one we have more content in the game)

---
+++ GameJam_CovidFighter/Assets/PauseMenuScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuScript : MonoBehaviour
+{
+
+    public string ExitSceneToGoTo;
+    public float waitTime = 0.4f;
+
+    public GameObject optionsMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //pauses
+        if (Input.GetKeyDown("escape"))
+        {
+            optionsMenu.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+    //unpauses
+    public void unpause()
+    {
+        optionsMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    //Takes User To A Specific Scene
+    public void restartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+
+    //Takes User To A Specific Scene
+    public void exitToScene()
+    {
+        SceneManager.LoadScene(ExitSceneToGoTo);
+    }
+
+}
