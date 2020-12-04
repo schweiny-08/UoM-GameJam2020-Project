aa9b5191fcfab6ceaf58597b26eb2fef03e18194
schweiny-08 <andrew.magri@outlook.com>
2020-12-04 19:22:38 +0100

    Created player shoot mechanic
    Added sanitizer drop sprite
    
    Added firepoint GameObject that changes position according to player orientation and rotates accurately to mouse position
    
    Drop detects wall and enemy

---
+++ GameJam_CovidFighter/Assets/Scripts/Player/DisinfectantScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisinfectantScript : MonoBehaviour
+{
+        void OnCollisionEnter2D(Collision2D col){
+        if(col.gameObject.tag == "Wall")
+            Destroy(gameObject);
+        else if(col.gameObject.tag == "Enemy"){
+            Debug.Log("ENEMY HIT");
+            Destroy(gameObject);
+        }
+    }
+}
