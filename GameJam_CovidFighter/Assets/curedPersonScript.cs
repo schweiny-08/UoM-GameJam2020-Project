7ad61760cdd8343b2a0599c0620d8968fde385d8
polple <66416994+polple@users.noreply.github.com>
2020-12-07 20:14:50 +0100

    Added cured people
    Added cured people prefabs (2 varients). They spawn after an enemy is cured and then follow the player and stop at the safe area where they then despawn with some effects after a designated amount of time

---
+++ GameJam_CovidFighter/Assets/curedPersonScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class curedPersonScript : MonoBehaviour
+{
+    public float destroyAfterTime = 4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "SafeZone")
+        {
+            //increment uninfected counter
+            GameObject.Find("GameMaster").GetComponent<GUI>().unifectedCounter++;
+            this.GetComponent<Animator>().SetBool("SafeArea",true);
+            StartCoroutine(HideAfter(destroyAfterTime));
+        }
+    }
+
+    IEnumerator HideAfter(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        this.GetComponent<SpriteRenderer>().enabled = false;
+        this.GetComponentInChildren<ParticleSystem>().Play();
+        StartCoroutine(DestroyAfter(1f)); //to give time for effects
+    }
+
+
+    IEnumerator DestroyAfter(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        Destroy(gameObject);
+    }
+}
